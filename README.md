bfcbe4a5051c24515c2c2fe290fa0e781bcfcdb6
Jarrod Parkes <jarrod@udacity.com>
2016-08-17 13:26:00 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This script takes 3 arguments:
+
+- mbox file: name of a mbox file in the executing directory
+- name of sender to filter: name in the FROM that you want to filter out
+- email of sender to filter: email in the FROM that you want to filter out
---
+++ mbox_parser.py
@@ -0,0 +1,39 @@
+import mailbox
+import csv
+
+# constants
+export_file_name = "clean_mail.csv"
+
+# get body of email
+def get_message(message):
+    if not message.is_multipart():
+        return message.get_payload()
+    contents = ""
+    for msg in message.get_payload():
+        contents = contents + str(msg.get_payload()) + '\n'
+    return contents
+
+if __name__ == "__main__":
+
+    # get command line arguments
+    mbox_file = raw_input("mbox file (ex. my_file.mbox): ")
+    name_of_sender = raw_input("name of sender to filter (ex. Jarrod Parkes): ")
+    email_of_sender = raw_input("email of sender to filter (ex. jarrod@udacity.com): ")
+
+    # create FROM filter
+    from_filter = name_of_sender + " <" + email_of_sender + ">"
+
+    # create CSV file
+    writer = csv.writer(open(export_file_name, "wb"))
+
+    # create header row
+    writer.writerow(["subject", "from", "date", "body"])
+
+    # add rows based on mbox file
+    for message in mailbox.mbox(mbox_file):
+        contents = get_message(message)
+        if message["from"] != from_filter:
+        	writer.writerow([message["subject"], message["from"], message["date"], contents])
+
+    # print finish message
+    print "generated csv file called " + export_file_name
