d830c870b61b43669934c0d098db23a08aa4ec59
Jarrod Parkes <parkesfjarrod@gmail.com>
2020-07-10 22:40:47 -0500

    add venv and capture new dependencies

--- mbox_parser.py
+++ mbox_parser.py
@@ -1,6 +1,7 @@
 import mailbox
 import unicodecsv as csv
-import html2text
+
+# review https://www.oreilly.com/library/view/mining-the-social/9781449368180/ch06.html
 
 # constants
 export_file_name = "clean_mail.csv"
@@ -9,15 +10,17 @@ export_file_name = "clean_mail.csv"
 def get_contents(email):
     if email.is_multipart():
         body = ""
-        for part in email.get_payload():
-            if part.is_multipart():
-                for subpart in part.walk():
-                    if subpart.get_content_type() == "text/html":
-                        body += str(subpart)
-                    elif subpart.get_content_type() == "text/plain":
-                        body += str(subpart)
-            else:
-                body += str(part)
+        parts = email.get_payload(decode=True)
+        if parts is not None:
+            for part in parts:
+                if part.is_multipart():
+                    for subpart in part.walk():
+                        if subpart.get_content_type() == "text/html":
+                            body += str(subpart)
+                        elif subpart.get_content_type() == "text/plain":
+                            body += str(subpart)
+                else:
+                    body += str(part)
         return body
     else:
         return email.get_payload()
@@ -31,13 +34,12 @@ if __name__ == "__main__":
     writer = csv.writer(open(export_file_name, "wb"), encoding='utf-8')
 
     # create header row
-    writer.writerow(["subject", "from", "date", "body"])
+    writer.writerow(["date", "from", "to", "cc", "subject", "content"])
 
     # add rows based on mbox file
     for email in mailbox.mbox(mbox_file):
         contents = get_contents(email)
-        contents = html2text.html2text(contents)
-        writer.writerow([email["subject"], email["from"], email["date"], contents])
+        writer.writerow([email["date"], email["from"], email["to"], email["cc"], email["subject"], contents])
 
     # print finish message
     print("generated CSV file called " + export_file_name)
