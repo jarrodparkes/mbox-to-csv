6ed2d70a8e6309a7122c3bac38c8bcb71ba18cf3
Jarrod Parkes <parkesfjarrod@gmail.com>
2020-07-22 05:08:46 -0500

    make rules its own file

---
+++ rules.example.py
@@ -0,0 +1,17 @@
+# external counts
+cant_convert_count = 0
+blacklist_count = 0
+
+# ["flagged", "date", "description", "from", "to", "cc", "subject", "content", "time (minutes)"]
+def apply_rules(date, sent_from, sent_to, cc, subject, contents, owner, blacklist_domains):
+    return [
+        "",
+        date,
+        "",
+        ", ".join(sent_from),
+        ", ".join(sent_to),
+        ", ".join(cc),
+        subject,
+        contents,
+        "0"
+    ]
